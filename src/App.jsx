34d26748ac1bc21ce3cb8c152e595ab2d5675c17
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2024-03-28 12:21:50 +0100

    Added state.1 branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,57 @@
 // import viteLogo from '/vite.svg'
 // import './App.css'
 import PokemonCard from "./components/PokemonCard"
+import { useState } from "react"
+
+// Liste des pokemons
+const pokemonList = [
+  {
+      name: "Bulbasaur",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+      name: "Mew",
+  },
+];
 
 function App() {
-  const pokemonList = [
-    {
-        name: "Bulbasaur",
-        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "Mew",
-    },
-  ];
+
+  // State pokmonIndex
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  // Fonctions pour changer de pokemon au click
+  const previousClick = () => {
+    setPokemonIndex(pokemonIndex - 1)
+  }
+  const nextClick = () => {
+    setPokemonIndex(pokemonIndex + 1)
+  }
 
   return (
     <div>
-        <PokemonCard pokemon={pokemonList[0]}/>
-        <PokemonCard pokemon={pokemonList[1]}/>
+        <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+        {pokemonIndex > 0 ? <button onClick={previousClick}>Previous</button> : ""}
+        {pokemonIndex < pokemonList.length - 1 ? <button onClick={nextClick}>Next</button> : ""}
     </div>
   );
 }
 
 export default App;
+
+// {indexPokemon > 0 ? <button onClick={previousClick}>Previous</button> : ""}
+// {indexPokemon < pokemonList.length - 1 ? <button onClick={nextClick}>Next</button> : ""}
+
+// <button onClick={previousClick}>Previous</button>
+// <button onClick={nextClick}>Next</button>
